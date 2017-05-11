068bf8f5462e64c847a860f83628640c1999a58e
jpd800 <jacob.duffy@fammed.wisc.edu>
2017-05-11 15:23:43 -0500

    Correctly tagging "host"

--- lib/rb2db/version.rb
+++ lib/rb2db/version.rb
@@ -1,8 +1,9 @@
 module Rb2db
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 
 
   # CHANGELOG
+  #   0.1.2 Tag 'host' correctly.  Works nicely in Grafana.
   #   0.1.1 Updating gemspec to publish to RubyGems
   #   0.1.0 Initial Release
 
