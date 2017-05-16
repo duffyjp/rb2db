aaddd8e97e4d92342f01eff4067e499f51617433
jpd800 <jacob.duffy@fammed.wisc.edu>
2017-05-16 15:45:13 -0500

    Added PID checking to prevent multiple instances.  Abort if INFLUX_HOST is not set.

--- lib/rb2db.rb
+++ lib/rb2db.rb
@@ -1,5 +1,19 @@
 require "rb2db/version"
 
 module Rb2db
-  # Your code goes here...
+  def self.pid_check
+
+    # PID file to use.
+    pid_file = ENV['XDG_RUNTIME_DIR'] || '/tmp' + '/.rb2db.pid'
+
+    # If the file exists, check if the pid inside is still running
+    if (pid = File.file?(pid_file) && File.read(pid_file))
+      if pid.to_i > 0 && `ps -p #{pid}`.include?(pid)
+        abort "[!] rb2db already running on pid #{pid}"
+      end
+    end
+
+    # Write new PID file.
+    File.write(pid_file, $$)
+  end
 end
--- lib/rb2db/version.rb
+++ lib/rb2db/version.rb
@@ -1,8 +1,9 @@
 module Rb2db
-  VERSION = "0.1.4"
+  VERSION = "0.2.0"
 
 
   # CHANGELOG
+  #   0.2.0 Added PID checking to prevent multiple instances.
   #   0.1.4 Replace mem_available with mem_percent.
   #   0.1.3 Bump 'duffy' gem version to fix mem_available detection on older linux
   #   0.1.2 Tag 'host' correctly.  Works nicely in Grafana.
