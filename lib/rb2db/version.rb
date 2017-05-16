63c8eb2e064e4ef1f5c944340710589935e132e8
jpd800 <jacob.duffy@fammed.wisc.edu>
2017-05-16 13:44:06 -0500

    Replace mem_available with mem_percent.  Nice for overlaying on a CPU chart for an AIO graph.

--- lib/rb2db/version.rb
+++ lib/rb2db/version.rb
@@ -1,8 +1,9 @@
 module Rb2db
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 
 
   # CHANGELOG
+  #   0.1.4 Replace mem_available with mem_percent.
   #   0.1.3 Bump 'duffy' gem version to fix mem_available detection on older linux
   #   0.1.2 Tag 'host' correctly.  Works nicely in Grafana.
   #   0.1.1 Updating gemspec to publish to RubyGems
--- rb2db.gemspec
+++ rb2db.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
 
-  spec.add_dependency 'duffy', '>= 0.3.1'
+  spec.add_dependency 'duffy', '>= 0.3.2'
   spec.add_dependency 'influxdb'
 
   spec.add_development_dependency "bundler", "~> 1.14"
